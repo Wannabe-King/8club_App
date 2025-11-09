181b8c64b2df86a35e9a5911a63e23eb98d8e70f
Priyanshu <sidarsunita2@gmail.com>
2025-11-09 17:52:29 +0530

    colors added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorX{
+  static const base1=Color(0xFF101010);
+  static const base2=Color(0xFF151515);
+  static const primaryAccent=Color(0xFF9196FF);
+  static const secondaryAccent=Color(0xFF5961FF);
+}
